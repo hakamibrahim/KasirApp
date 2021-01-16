ca7deadfd058e13c7994971c8b954676dd2b2bc5
hakamibrahim <syah.hakam@jappri.id>
2021-01-16 15:04:04 +0700

    endpoint save

--- app/src/main/java/com/hakam/kasirapp/network/ApiEndpoint.kt
+++ app/src/main/java/com/hakam/kasirapp/network/ApiEndpoint.kt
@@ -3,6 +3,7 @@ package com.hakam.kasirapp.network
 import com.hakam.kasirapp.model.DetailPenjualanModel
 import com.hakam.kasirapp.model.ListPenjualanModel
 import com.hakam.kasirapp.model.ListProdukModel
+import com.hakam.kasirapp.model.SaveProdukModel
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -20,5 +21,14 @@ interface ApiEndpoint {
     fun getDetail(
         @Query("id") id: Int,
     ): Call<List<DetailPenjualanModel>>
+
+    @Headers("x-key: bd0b3ae6651538fac2515baafc9326c5")
+    @POST("save")
+    fun saveProduk(
+        @Field("id_produk") id_produk1: ArrayList<Int>,
+        @Field("qty") qty1: ArrayList<String>,
+        @Field("id_produk") id_produk2: ArrayList<Int>,
+        @Field("qty") qty2: ArrayList<String>
+    ): Call<SaveProdukModel>
 
 }
